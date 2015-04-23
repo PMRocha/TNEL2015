6c54a2e43c0e59167fcd9921d2603b69abde6f8c
ruigames <rui_games@hotmail.com>
2015-04-23 19:08:03 +0100

    Main Container created

--- DutchAuctionHouse/src/project/Main.java
+++ DutchAuctionHouse/src/project/Main.java
@@ -1,7 +1,34 @@
 package project;
 
+import java.util.Scanner;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
 public class Main {
 	public static void main(String arg[]){
-		System.out.println("Hello World");
+		Runtime rt = Runtime.instance();
+		
+		Profile p = new ProfileImpl();
+		// Create a new non-main container, connecting to the default
+		// main container (i.e. on this host, port 1099)
+		ContainerController cc = rt.createMainContainer(p);
+		
+		
+		
+		try {
+			AgentController rma = cc.createNewAgent("rma",
+					"jade.tools.rma.rma", null);
+
+			rma.start();
+
+		} catch (StaleProxyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
