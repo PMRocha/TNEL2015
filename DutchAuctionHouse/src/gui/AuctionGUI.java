0e3c82b5a876d16b0c594a1f0af64d12cd3878ee
PMRocha <ihastealth@gmail.com>
2015-04-30 19:18:02 +0100

    initial interface

---
+++ DutchAuctionHouse/src/gui/AuctionGUI.java
@@ -0,0 +1,24 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+
+public class AuctionGUI extends JFrame implements ActionListener{
+	public AuctionGUI() {
+	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
