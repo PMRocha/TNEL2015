9807eab86e9b24f19abdcc4d6ad7682f4103c323
PMRocha <ei11078@fe.up.pt>
2015-05-02 15:15:32 +0100

    advances in the UI

---
+++ DutchAuctionHouse/src/gui/AuctionGUI.java
@@ -0,0 +1,31 @@
+package gui;
+ 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class AuctionGUI extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    	
+    	Parent root = FXMLLoader.load(getClass().getResource("AuctionGUI.fxml"));
+        
+        Scene scene = new Scene(root, 800, 800);
+    
+        primaryStage.setTitle("Auction");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
